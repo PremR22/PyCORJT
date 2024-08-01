89b1fc7a3754900df7f51aabf5b40117b4ee7da8
PremR <premrastogi2210@gmail.com>
2024-08-01 18:57:46 +0530

    added css

--- OCRproject/settings.py
+++ OCRproject/settings.py
@@ -117,10 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+
+STATIC_ROOT = 'staticfiles/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    BASE_DIR / 'static/'
 ]
 
 # Default primary key field type
