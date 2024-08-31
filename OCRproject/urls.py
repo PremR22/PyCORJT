4d581741d299516d096d25408c56484ffda9e8c8
PremR <premrastogi2210@gmail.com>
2024-08-31 15:27:24 +0530

    changed url

--- OCRproject/urls.py
+++ OCRproject/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ocr/', include('OCRapp.urls')),
+    path('', include('OCRapp.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
